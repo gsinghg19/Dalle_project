889ae0f9a0ccfdb42a4ea1f717d779ce37e02532
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-04-28 15:42:13 +0100

    feature: added form section and buttons for user input prompt section.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/Header";
 import "../styles/globals.css";
+import PromptInput from "@/components/PromptInput";
 
 export const metadata = {
   title: "Create Next App",
@@ -14,10 +15,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        {/*header*/}
         <Header />
-
-        {/*Prompt input*/}
+        <PromptInput />
         {children}
       </body>
     </html>
---
+++ components/PromptInput.tsx
@@ -0,0 +1,40 @@
+"use client";
+
+import { useState } from "react";
+
+function PromptInput() {
+  const [input, setInput] = useState("");
+
+  return (
+    <div className="m-10">
+      <form className="flex flex-col lg:flex-row shadow-xl shadow-slate-400/30 border rounded-md lg:divide-x ">
+        <textarea
+          value={input}
+          onChange={(e) => {
+            setInput(e.target.value);
+          }}
+          placeholder="Enter a prompt"
+          className="flex-1 p-4 outline-none rounded-md shadow-inner shadow-slate-400/40"
+        />
+        <button
+          type="submit"
+          className={`p-4 ${
+            input
+              ? "bg-violet-500 text-white transition-colors duration-200"
+              : "text-gray-300 cursor-not-allowed"
+          }`}
+        >
+          Generate
+        </button>
+        <button className="p-4 bg-violet-400 text-white transition-colors duration-200 font-bold disabled:text-gray-300 disabled:cursor-not-allowed disabled:bg-gray-400">
+          Use Suggestion
+        </button>
+        <button className="p-4 bg-white text-violet-600 transition-colors duration-200 font-bold disabled:text-gray-300 disabled:cursor-not-allowed disabled:bg-gray-400">
+          New Suggestion
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default PromptInput;
