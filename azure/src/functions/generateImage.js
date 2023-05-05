88d793cc827a649fe7af656e62bc6ce817453259
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-05-05 14:37:53 +0100

    feature: generate images from open ai model, and store them in the images container on azure storage client

--- azure/src/functions/generateImage.js
+++ azure/src/functions/generateImage.js
@@ -2,10 +2,10 @@ const { app } = require("@azure/functions");
 const openai = require("../../lib/openai");
 const axios = require("axios");
 const generateSASToken = require("../../lib/generateSASToken");
-const { BlobServiceClient } = require("../../lib/generateSASToken0");
+const { BlobServiceClient } = require("@azure/storage-blob");
 
 const accountName = process.env.accountName;
-const containerName = process.env.containerName;
+const containerName = "images";
 
 function newAbortSignal(timeoutMs) {
   const abortController = new AbortController();
@@ -15,29 +15,55 @@ function newAbortSignal(timeoutMs) {
 }
 
 app.http("generateImage", {
-  methods: [""],
+  methods: ["POST"],
   authLevel: "anonymous",
-  handler: async (request, response) => {
+  handler: async (request) => {
+    const { prompt } = await request.json();
+    console.log(`Prompt is ====> ${prompt}`);
+
+    const response = await openai.createImage(
+      {
+        prompt: prompt,
+        n: 1,
+        size: "1024x1024",
+      },
+      {
+        timeout: 20000,
+        signal: newAbortSignal(17000),
+      }
+    );
+
+    //download image data here
+    image_url = response.data.data[0].url;
+    const res = await axios.get(image_url, { responseType: "arraybuffer" });
+    const arrayBuffer = res.data;
+
+    //accessing azure storage
+    sasToken = await generateSASToken();
+    const blobServiceClient = new BlobServiceClient(
+      `https://${accountName}.blob.core.windows.net?${sasToken}`
+    );
+
+    //access image container in azure storage via query
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    //generate timestamp
+    const timestamp = new Date().getTime();
+    const file_name = `${prompt}_${timestamp}.png`;
+    const blockBlobClient = containerClient.getBlockBlobClient(file_name);
+
     try {
-      const response = await openai.createCompletion(
-        {
-          //model:
-          //prompt:
-          //max_tokens:
-          //temperature:
-        },
-        {
-          timeout: 7000,
-          signal: newAbortSignal(5000),
-        }
-      );
+      await blockBlobClient.uploadData(arrayBuffer);
+      console.log("Success! File uploaded.");
     } catch (error) {
       if (error.response) {
+        console.log("Error has occured. Upload failed...");
         console.log(error.response.status);
         console.log(error.response.data);
       } else {
-        console.log(error.message);
+        console.log("Error has occured. Upload failed... ::", error.message);
       }
+      return { body: "Successfully uploaded image" };
     }
   },
 });
