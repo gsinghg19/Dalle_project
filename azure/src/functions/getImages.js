d0e78c49f9b66548e94047e09830493467148530
Gurpreet Singh <91670399+gsinghg19@users.noreply.github.com>
2023-05-05 17:17:38 +0100

    feature: adding getImages azure fucntion. To finish this, manipulate url string to gain teimstamp data, to help in sorting files.

--- azure/src/functions/getImages.js
+++ azure/src/functions/getImages.js
@@ -1,5 +1,18 @@
 const { app } = require("@azure/functions");
 const openai = require("../../lib/openai");
+const generateSASToken = require("../../lib/generateSASToken");
+const { BlobServiceClient } = require("@azure/storage-blob");
+
+const accountName = process.env.accountName;
+const accountKey = process.env.accountKey;
+const containerName = "images";
+
+const sharedKeyCredential = new StorageSharedKeyClient(accountName, accountKey);
+
+const blobServiceClient = new BlobServiceClient(
+  `https://${accountName}.blob.core.windows.net`,
+  sharedKeyCredential
+);
 
 function newAbortSignal(timeoutMs) {
   const abortController = new AbortController();
@@ -11,5 +24,18 @@ function newAbortSignal(timeoutMs) {
 app.http("getImages", {
   methods: ["GET"],
   authLevel: "anonymous",
-  handler: async (request, context) => {},
+  handler: async (request, context) => {
+    const containerClient = blobServiceClient.getContainerClient(containerName);
+
+    const imageUrlsArr = [];
+    const sasToken = await generateSASToken();
+
+    for await (const blob of containerClient.listBlobsFlat()) {
+      const imageUrl = `${blob.name}?${sasToken}`;
+      const url = `https://${accountName}.blob.core.windows.net/images/${imageUrl}`;
+      imageUrlsArr.push({ url, name: blob.name });
+    }
+
+    //TODO: Now split image filenames string (filename is structured as `${fileName}_${timestamp.png}`) to obtain timestamp. Use timestamp to sort images.
+  },
 });
